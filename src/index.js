ab63002c377ef958abaca1d75241d384753cc6d6
Ben Smith <benjaminwrobelsmith@gmail.com>
2022-07-06 13:58:24 -0500

    incorporated local storage functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,70 @@
-const table = document.querySelector('.history_table')
+const table = document.querySelector(".history_table");
+const form = document.querySelector("#entry_form");
+const info = document.querySelector(".general_info");
+const name = document.querySelector("#name");
+const mpg = document.querySelector("#mpg");
+const clear = document.querySelector(".clear");
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        var row = table.insertRow();
-        var cell = row.insertCell();
-        cell.innerHTML = request.site;
-    }
-);
+function round(num) {
+  return +(Math.round(num + "e+2") + "e-2");
+}
+
+function init() {
+  var saved_mpg = localStorage.getItem("mpg");
+  var user_name = localStorage.getItem("name");
+  var new_user;
+  if (saved_mpg === null) {
+    new_user = true;
+    console.log("No data found in localStorage");
+  } else {
+    new_user = false;
+    console.log(
+      `Got value ${saved_mpg} mpg and name ${user_name} from localStorage`
+    );
+  }
+  if (new_user) {
+    form.style.display = "block";
+    info.style.display = "none";
+  } else {
+    showWelcomeMessage(user_name, saved_mpg);
+  }
+}
+
+function showWelcomeMessage() {
+  form.style.display = "none";
+  info.style.display = "block";
+  if (name === null) {
+    name = "user";
+  }
+  document.getElementById("welcome_name").textContent =
+    localStorage.getItem("name") + "!";
+  document.getElementById("welcome_mpg").textContent =
+    "Your saved MPG is " + localStorage.getItem("mpg") + " miles per gallon.";
+  document.getElementById("sample_trip").textContent = "$" + calculateCost(10);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log(`Received ${name.value} and ${mpg.value} from form submission`);
+  localStorage.setItem("mpg", mpg.value);
+  localStorage.setItem("name", name.value);
+  showWelcomeMessage();
+}
+
+function calculateCost(distance) {
+  var currentGasPrice = 4.5;
+  var percent = distance / localStorage.getItem("mpg");
+
+  return (percent * currentGasPrice).toFixed(2);
+}
+
+function reset(event) {
+  event.preventDefault();
+  console.log("Clearing localStorage");
+  localStorage.clear();
+  init();
+}
+
+form.addEventListener("submit", (e) => handleSubmit(e));
+clear.addEventListener("click", (e) => reset(e));
+init();
