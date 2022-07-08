d86f8b2412be96f1b66cd84c99be63c4db213bfa
Ben Smith <benjaminwrobelsmith@gmail.com>
2022-07-08 12:14:15 -0500

    background script is updated when user changes MPG to reflect changes upon new side load

--- dist/background.js
+++ dist/background.js
@@ -2,15 +2,20 @@ var saved_mpg = -10000;
 
 function calculateCost(distance) {
     var currentGasPrice = 4.5;
-    //var percent = distance / localStorage.getItem("mpg");
+    var percent = distance / saved_mpg;
   
-    var percent = distance / 24; 
     console.log(`A trip of ${distance} will cost ${(percent * currentGasPrice).toFixed(2)}`)
     return (percent * currentGasPrice).toFixed(2);
   }
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
+        if (request.data.includes("MPG")) {
+            saved_mpg = parseFloat(request.data.slice(4));
+            console.log(`Received MPG ${request.data} message`);
+            console.log(`New saved_mpg is ${saved_mpg}`);
+            return;
+        }
       let ret_vals = [];
       var recvd = request.data.split(',');
       for (idx in recvd) {
