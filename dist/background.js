a761cec5ce6378f25df779b5f7d953f7a74fa32a
Ben Smith <benjaminwrobelsmith@gmail.com>
2023-01-10 20:31:14 -0600

    removed legacy code, left chrome storage alert for now for debugging. This file will be removed completely upon release

--- dist/background.js
+++ dist/background.js
@@ -1,51 +1,4 @@
-var saved_mpg = -10000;
-
-function calculateCost(distance) {
-    console.log(`calculateCost() with distance as ${distance} and mpg as ${saved_mpg}`);
-    if (saved_mpg < 0) {
-        console.log("There was an error");
-        throw "NegativeMPG"
-    }
-    var currentGasPrice = 4.0;
-    var percent = distance / saved_mpg;
-  
-    return (percent * currentGasPrice).toFixed(2);
-}
-
-const getMPG = async (key) => {
-    return new Promise((resolve, reject) => {
-      chrome.storage.sync.get([key], function (result) {
-        if (result[key] === undefined) {
-    
-          reject();
-        } else {
-
-          resolve(result[key]);
-        }
-      });
-    });
-  };
-
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    //saved_mpg = getMPG("mpg");
-    //saved_mpg = parseInt(saved_mpg);
-    let recvd = [];
-    let calculated = [];
-    recvd = message.data.split(",");
-    try {
-        for (const val of recvd) {
-            var dist = val.split(" ")[0];
-            dist = parseFloat(dist);
-            let c = calculateCost(dist);
-            calculated.push(c);
-        }
-        console.log(calculated);
-        sendResponse({cost:`${calculated}`});
-    } catch (error) {
-        console.log(error);
-    }
-});
+// this file is legacy - now it just console logs information about the chrome storage
 
 chrome.storage.onChanged.addListener(function (changes, namespace) {
     for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
@@ -56,6 +9,5 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
       if (key.toString() === "mpg"){
         saved_mpg = parseInt(newValue);
       }
-      
     }
   });
